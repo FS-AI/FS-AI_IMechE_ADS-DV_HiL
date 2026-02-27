48e39d924cc36df5463de1b89752c6d1cb8060f1
Ian Murphy <ian.murphy@member.imeche.org>
2026-02-27 18:12:16 +0000

    WIP further clean-up & deleting 'Full' files as not needed.

--- .CarMaker.tcl
+++ .CarMaker.tcl
@@ -5,7 +5,10 @@
 ## Loads the provided basic test run at startup.
 
 Project::CheckProject
+
+## If the following line is not present, and the Test Run 'FS_autonomous_Straight_001' is not available, a FATAL ERROR results...
 LoadTestRun "FS_autonomous_Straight_001"
+
 ##::CMRosIF::ROS_Launch -mode launch
 Application connect
 ##Application start
