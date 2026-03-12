a162dcd5ad52e03ee2d94ef3d578c99270749342
Ian Murphy <ian.murphy@member.imeche.org>
2026-03-12 19:53:06 +0000

    WIP importing from LTS.

--- Data/Script/generate_cones.tcl
+++ Data/Script/generate_cones.tcl
@@ -1,113 +1,101 @@
-proc addLargeOrangeCone {fileID n s t} {
+proc addLargeOrangeCone {fileID n id s t} {
 	# Add large organe cone to the opened infofile with handle fileID
 	# The cone is added as a traffic object number $n
 	# The cone is added to the main route at offset ($s %t)
-    puts $fileID "Traffic.$n.ObjectKind = StatWithName"
-	puts $fileID "Traffic.$n.ObjectClass = Unknown"
 	puts $fileID "Traffic.$n.Name = CN$n"
-	puts $fileID "Traffic.$n.Info = Large Orange Start Cone"
-	puts $fileID "Traffic.$n.Movie.Geometry = TrafficCones/TrafficCone_Large_Orange.obj"
-	puts $fileID "Traffic.$n.Color = 1.0 0.65 0.0"
-	puts $fileID "Traffic.$n.Basics.Dimension = 0.285 0.285 0.505"
-	puts $fileID "Traffic.$n.Basics.Offset = 0.0 0.0"
-	puts $fileID "Traffic.$n.Basics.Fr12CoM = 0.143"
-	puts $fileID "Traffic.$n.Init.Orientation = 0.0 0.0 0.0"
-	puts $fileID "Traffic.$n.RCSClass = RCS_GuardRailPost"
 	puts $fileID "Traffic.$n.DetectMask = 1 1"
-	puts $fileID "Traffic.$n.Route = 0 1"
-	puts $fileID "Traffic.$n.Init.Road = $s $t"
-	puts $fileID "Traffic.$n.Init.v = 0.0"
-	puts $fileID "Traffic.$n.FreeMotion = 0"
-	puts $fileID "Traffic.$n.Lighting = 0"
 	puts $fileID "Traffic.$n.UpdRate = 200"
-	puts $fileID "Traffic.$n.Motion.Kind = Course"
-	puts $fileID "Traffic.$n.Motion.mass = 1.05"
+	puts $fileID "Traffic.$n.AutoDrv.UpdRate = 200"
+	puts $fileID "Traffic.$n.Lighting = 0"
+	puts $fileID "Traffic.$n.FreeMotion = 0"
+	puts $fileID "Traffic.$n.TrailerName = "
+	puts $fileID "Traffic.$n.Template.FName = TrafficCone_Large_Orange"
+	puts $fileID "Traffic.$n.AutoDriver.FName = "
+	puts $fileID "Traffic.$n.Routing.Type = Route"
+	puts $fileID "Traffic.$n.Routing.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos.Type = Route"
+	puts $fileID "Traffic.$n.StartPos.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos = $s $t"
+	puts $fileID "Traffic.$n.StartPos.Orientation.Type = Relative"
+	puts $fileID "Traffic.$n.StartPos.Orientation = 0.0 0.0 0.0"
+	puts $fileID "Traffic.$n.nMan = 0"
+	puts $fileID "Traffic.$n.Man.Start.Velocity = 0.0"
 	puts $fileID "Traffic.$n.Man.TreatAtEnd = FreezePos"
-	puts $fileID "Traffic.$n.Man.N = 0"
 }
 
-proc addOrangeCone {fileID n s t} {
+proc addOrangeCone {fileID n id s t} {
 	# Add small organe cone to the opened infofile with handle fileID
 	# The cone is added as a traffic object number $n
 	# The cone is added to the main route at offset ($s %t)
-    puts $fileID "Traffic.$n.ObjectKind = StatWithName"
-	puts $fileID "Traffic.$n.ObjectClass = Unknown"
-	puts $fileID "Traffic.$n.Name = CN$n"
-	puts $fileID "Traffic.$n.Info = Small Orange Exit/Entry Lane Cone"
-	puts $fileID "Traffic.$n.Movie.Geometry = TrafficCones/TrafficCone_Small_Orange.obj"
-	puts $fileID "Traffic.$n.Color = 1.0 0.65 0.0"
-	puts $fileID "Traffic.$n.Basics.Dimension = 0.228 0.228 0.325"
-	puts $fileID "Traffic.$n.Basics.Offset = 0.0 0.0"
-	puts $fileID "Traffic.$n.Basics.Fr12CoM = 0.114"
-	puts $fileID "Traffic.$n.Init.Orientation = 0.0 0.0 0.0"
-	puts $fileID "Traffic.$n.RCSClass = RCS_GuardRailPost"
+    puts $fileID "Traffic.$n.Name = CN$n"
 	puts $fileID "Traffic.$n.DetectMask = 1 1"
-	puts $fileID "Traffic.$n.Route = 0 1"
-	puts $fileID "Traffic.$n.Init.Road = $s $t"
-	puts $fileID "Traffic.$n.Init.v = 0.0"
-	puts $fileID "Traffic.$n.FreeMotion = 0"
-	puts $fileID "Traffic.$n.Lighting = 0"
 	puts $fileID "Traffic.$n.UpdRate = 200"
-	puts $fileID "Traffic.$n.Motion.Kind = Course"
-	puts $fileID "Traffic.$n.Motion.mass = 0.45"
+	puts $fileID "Traffic.$n.AutoDrv.UpdRate = 200"
+	puts $fileID "Traffic.$n.Lighting = 0"
+	puts $fileID "Traffic.$n.FreeMotion = 0"
+	puts $fileID "Traffic.$n.TrailerName = "
+	puts $fileID "Traffic.$n.Template.FName = TrafficCone_Small_Orange"
+	puts $fileID "Traffic.$n.AutoDriver.FName = "
+	puts $fileID "Traffic.$n.Routing.Type = Route"
+	puts $fileID "Traffic.$n.Routing.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos.Type = Route"
+	puts $fileID "Traffic.$n.StartPos.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos = $s $t"
+	puts $fileID "Traffic.$n.StartPos.Orientation.Type = Relative"
+	puts $fileID "Traffic.$n.StartPos.Orientation = 0.0 0.0 0.0"
+	puts $fileID "Traffic.$n.nMan = 0"
+	puts $fileID "Traffic.$n.Man.Start.Velocity = 0.0"
 	puts $fileID "Traffic.$n.Man.TreatAtEnd = FreezePos"
-	puts $fileID "Traffic.$n.Man.N = 0"
 }
 
-proc addBlueCone {fileID n s t} {
+proc addBlueCone {fileID n id s t} {
 	# Add small blue cone to the opened infofile with handle fileID
 	# The cone is added as a traffic object number $n
 	# The cone is added to the main route at offset ($s %t)
-    puts $fileID "Traffic.$n.ObjectKind = StatWithName"
-	puts $fileID "Traffic.$n.ObjectClass = Unknown"
 	puts $fileID "Traffic.$n.Name = CN$n"
-	puts $fileID "Traffic.$n.Info = Left Blue Cone"
-	puts $fileID "Traffic.$n.Movie.Geometry = TrafficCones/TrafficCone_Small_Blue.obj"
-	puts $fileID "Traffic.$n.Color = 0 0 1"
-	puts $fileID "Traffic.$n.Basics.Dimension = 0.228 0.228 0.325"
-	puts $fileID "Traffic.$n.Basics.Offset = 0.0 0.0"
-	puts $fileID "Traffic.$n.Basics.Fr12CoM = 0.114"
-	puts $fileID "Traffic.$n.Init.Orientation = 0.0 0.0 0.0"
-	puts $fileID "Traffic.$n.RCSClass = RCS_GuardRailPost"
 	puts $fileID "Traffic.$n.DetectMask = 1 1"
-	puts $fileID "Traffic.$n.Route = 0 1"
-	puts $fileID "Traffic.$n.Init.Road = $s $t"
-	puts $fileID "Traffic.$n.Init.v = 0.0"
-	puts $fileID "Traffic.$n.FreeMotion = 0"
-	puts $fileID "Traffic.$n.Lighting = 0"
 	puts $fileID "Traffic.$n.UpdRate = 200"
-	puts $fileID "Traffic.$n.Motion.Kind = Course"
-	puts $fileID "Traffic.$n.Motion.mass = 0.45"
+	puts $fileID "Traffic.$n.AutoDrv.UpdRate = 200"
+	puts $fileID "Traffic.$n.Lighting = 0"
+	puts $fileID "Traffic.$n.FreeMotion = 0"
+	puts $fileID "Traffic.$n.TrailerName = "
+	puts $fileID "Traffic.$n.Template.FName = TrafficCone_Small_Blue"
+	puts $fileID "Traffic.$n.AutoDriver.FName = "
+	puts $fileID "Traffic.$n.Routing.Type = Route"
+	puts $fileID "Traffic.$n.Routing.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos.Type = Route"
+	puts $fileID "Traffic.$n.StartPos.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos = $s $t"
+	puts $fileID "Traffic.$n.StartPos.Orientation.Type = Relative"
+	puts $fileID "Traffic.$n.StartPos.Orientation = 0.0 0.0 0.0"
+	puts $fileID "Traffic.$n.nMan = 0"
+	puts $fileID "Traffic.$n.Man.Start.Velocity = 0.0"
 	puts $fileID "Traffic.$n.Man.TreatAtEnd = FreezePos"
-	puts $fileID "Traffic.$n.Man.N = 0"
 }
 
-proc addYellowCone {fileID n s t} {
+proc addYellowCone {fileID n id s t} {
 	# Add small yellow cone to the opened infofile with handle fileID
 	# The cone is added as a traffic object number $n
 	# The cone is added to the main route at offset ($s %t)
-    puts $fileID "Traffic.$n.ObjectKind = StatWithName"
-	puts $fileID "Traffic.$n.ObjectClass = Unknown"
-	puts $fileID "Traffic.$n.Name = CN$n"
-	puts $fileID "Traffic.$n.Info = Right Yellow Cone"
-	puts $fileID "Traffic.$n.Movie.Geometry = TrafficCones/TrafficCone_Small_Yellow.obj"
-	puts $fileID "Traffic.$n.Color = 1 1 0"
-	puts $fileID "Traffic.$n.Basics.Dimension = 0.228 0.228 0.325"
-	puts $fileID "Traffic.$n.Basics.Offset = 0.0 0.0"
-	puts $fileID "Traffic.$n.Basics.Fr12CoM = 0.114"
-	puts $fileID "Traffic.$n.Init.Orientation = 0.0 0.0 0.0"
-	puts $fileID "Traffic.$n.RCSClass = RCS_GuardRailPost"
+    puts $fileID "Traffic.$n.Name = CN$n"
 	puts $fileID "Traffic.$n.DetectMask = 1 1"
-	puts $fileID "Traffic.$n.Route = 0 1"
-	puts $fileID "Traffic.$n.Init.Road = $s $t"
-	puts $fileID "Traffic.$n.Init.v = 0.0"
-	puts $fileID "Traffic.$n.FreeMotion = 0"
-	puts $fileID "Traffic.$n.Lighting = 0"
 	puts $fileID "Traffic.$n.UpdRate = 200"
-	puts $fileID "Traffic.$n.Motion.Kind = Course"
-	puts $fileID "Traffic.$n.Motion.mass = 0.45"
+	puts $fileID "Traffic.$n.AutoDrv.UpdRate = 200"
+	puts $fileID "Traffic.$n.Lighting = 0"
+	puts $fileID "Traffic.$n.FreeMotion = 0"
+	puts $fileID "Traffic.$n.TrailerName = "
+	puts $fileID "Traffic.$n.Template.FName = TrafficCone_Small_Yellow"
+	puts $fileID "Traffic.$n.AutoDriver.FName = "
+	puts $fileID "Traffic.$n.Routing.Type = Route"
+	puts $fileID "Traffic.$n.Routing.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos.Type = Route"
+	puts $fileID "Traffic.$n.StartPos.ObjId = $id"
+	puts $fileID "Traffic.$n.StartPos = $s $t"
+	puts $fileID "Traffic.$n.StartPos.Orientation.Type = Relative"
+	puts $fileID "Traffic.$n.StartPos.Orientation = 0.0 0.0 0.0"
+	puts $fileID "Traffic.$n.nMan = 0"
+	puts $fileID "Traffic.$n.Man.Start.Velocity = 0.0"
 	puts $fileID "Traffic.$n.Man.TreatAtEnd = FreezePos"
-	puts $fileID "Traffic.$n.Man.N = 0"
 }
 
 
@@ -119,7 +107,7 @@ proc cleanUp {fileName} {
 	set fc [read $fileID]
 	
 	# Seek all Traffic.NUM lines and delete them
-	regsub -all -line {Traffic\.\d.+\n} $fc {""} fc
+	regsub -all -line {Traffic\.\d.+\n} $fc {} fc
 	
 	# Re-open file and clear its contents
 	set fileID [open Data/TestRun/$fileName w]
@@ -138,8 +126,9 @@ file copy -force Data/TestRun/$fileName $fileNameBak
 # Clean ALL traffic objects from the TestRun
 cleanUp $fileName
 
-# Get the route length and set the number of cones accordingly
-set routeLen [IFileValue TestRun Road.Route.0.Length]
+# Get the route length+ID and set the number of cones accordingly
+set routeLen [IFileValue Road Route.0.Length]
+set routeID [IFileValue Road Route.0.ID]
 set coneDist 5
 set nCones [expr "int($routeLen/$coneDist)"]
 
@@ -150,18 +139,17 @@ IFileModify TestRun Traffic.N [expr "2*$nCones+4"]
 set fileID [open Data/TestRun/$fileName a]
 
 # Add 2x2 large orange cones at the start
-addLargeOrangeCone $fileID 0 0 1.5
-addLargeOrangeCone $fileID 1 0 -1.5
-addLargeOrangeCone $fileID 2 0.5 1.5
-addLargeOrangeCone $fileID 3 0.5 -1.5
+addLargeOrangeCone $fileID 0 $routeID 0 1.5
+addLargeOrangeCone $fileID 1 $routeID 0 -1.5
+addLargeOrangeCone $fileID 2 $routeID 0.5 1.5
+addLargeOrangeCone $fileID 3 $routeID 0.5 -1.5
 
 # Add blue/yellow cones on the left/right side of the track
 for {set i 0} {$i < $nCones} {incr i} {
-    addBlueCone $fileID [expr "2*$i+4"] [expr "5*$i+5"] 1.5
-	addYellowCone $fileID [expr "2*$i+5"] [expr "5*$i+5"] -1.5
+    addBlueCone $fileID [expr "2*$i+4"] $routeID [expr "5*$i+5"] 1.5
+	addYellowCone $fileID [expr "2*$i+5"] $routeID [expr "5*$i+5"] -1.5
 }
 
 # Close and flush the IFile to apply changes
 close $fileID
 IFileFlush
-
